4179916025caf3c8ddddaa8a55c65938d8bb8c7d
artur940617 <artur940617@gmail.com>
2022-10-22 01:15:40 +0400

    fixed bug

--- bonus_project.py
+++ bonus_project.py
@@ -2,25 +2,29 @@ import datetime
 import time
 
 
-
-
-p = 0
 st = input('Insert time to count down (h:m:s) ')
+if int(st.split(':')[0]) > 23:
+    raise Exception("Hour is too large. ENTER AN HOUR LESS THAN 24!!!")
+
+h, m, s = 0, 0, 0
 date_time_obj = datetime.datetime.strptime(st, '%H:%M:%S')
-m = datetime.datetime.strptime('0:0:0', '%H:%M:%S')
 
 while True:
-
-        s = datetime.datetime.strptime('0:0:{}'.format(p), '%H:%M:%S')
-        if date_time_obj >= s:
-                p += 1
-                x = date_time_obj - s
-                z = time.strftime("%H:%M:%S")
-                print(x)
-                time.sleep(1)
-        else:
-                print('Time is over !')
-                break
+    if s > 59:
+        s = 0
+        m = m + 1
+    if m > 59:
+        m = 0
+        h = h + 1
+    l = datetime.datetime.strptime('{}:{}:{}'.format(h, m, s), '%H:%M:%S')
+    if date_time_obj >= l:
+        s += 1
+        x = date_time_obj - l
+        print(x)
+        time.sleep(1)
+    else:
+        print('Time is over !')
+        break
 
 
 
